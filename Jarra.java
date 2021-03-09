6a88dd5d877059d39d2f65dd7af1fa3c408511e0
miguelog-uma <miguelog@uma.es>
2021-03-09 19:36:58 +0100

    Codigo opcional

--- Jarra.java
+++ Jarra.java
@@ -29,4 +29,18 @@ public class Jarra {//variables de instancia
     public bool vacia(){
         return this.cantidad=0;
     }
+
+    public Object clone() throws CloneNotSupportedException {
+        return (Jarra) super.clone();
+    }
+    public bool equals(Jarra otraJarra){
+        bool eqs = false;
+        if (null == otraJarra){ eqs = false; }
+        else if (this == otraJarra){ eqs = true; }
+        else if (otraJarra instanceOf Jarra.class){
+            eqs = this.capacidad == otraJarra.capacidad &&
+                    this.cantidad == otraJarra.cantidad;
+        } else { eqs = false; }
+        return eqs;
+    }
 }
